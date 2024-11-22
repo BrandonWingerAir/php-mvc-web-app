50f9777cc083f6a535ebe8664d02aaf24354c4e4
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-21 23:50:11 -0500

    header, footer and titles text content

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
+<?php include('view/header.php') ?>
 <h1>MVC Web Application</h1>
+<?php include('view/footer.php') ?>
---
+++ view/footer.php
@@ -0,0 +1,4 @@
+    </div>
+    <h6 align="center">Brandon Winger-Air © 2024 | Built using PHP: Hypertext Preprocessor</h6>
+</body>
+</html>
